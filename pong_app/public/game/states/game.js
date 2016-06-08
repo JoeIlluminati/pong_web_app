6e18f1440b02a021565b87130b9b9db8b7b84999
Joe Illuminati <illumijo@gmail.com>
2016-06-08 00:38:59 -0400

    Set up systems properly, everything seems to be working well so far!

--- pong_app/public/game/states/game.js
+++ pong_app/public/game/states/game.js
@@ -1,10 +1,14 @@
 Pong.Game = function(game)
 {
+    // Game objects
     this.ball;
     this.player1;
     this.player2;
+    // Game Score
     this.player1_score;
     this.player2_score;
+    // Game World
+    this.world;
 };
 Pong.Game.prototype = 
 {
@@ -28,7 +32,7 @@ Pong.Game.prototype =
         // Player 1
         this.player1 = new CES.Entity();
         this.player1.addComponent(new Pong.Components.Position(100, 100));
-        this.player1.addComponent(new Pong.Components.Velocity(0, 0));
+        this.player1.addComponent(new Pong.Components.Velocity(25, 0));
         this.player1.addComponent(new Pong.Components.Graphic(
             this.add.image(100, 100, 'paddle_1')));
         
@@ -39,10 +43,72 @@ Pong.Game.prototype =
         this.player2.addComponent(new Pong.Components.Graphic(
             this.add.image(700, 100, 'paddle_2')));
         console.log('Let the games begin!');
+        
+        /**
+         * Define the systems that will be updated on each
+         * tick of the game.
+         */
+        var InputSystem = CES.System.extend({
+            update: function(dt)
+            {
+
+            }
+        });
+
+        var MovementSystem = CES.System.extend({
+            update: function (dt)
+            {
+                var entities, pos, vel;
+                entities = this.world.getEntities('position', 'velocity');
+
+                entities.forEach(function (entity) {
+                    pos = entity.getComponent('position');
+                    vel = entity.getComponent('velocity');
+                    pos.x += vel.x * dt;
+                    pos.y += vel.y * dt;
+                });
+            }
+        });
+
+        var GraphicSystem = CES.System.extend({
+            update: function (dt)
+            {
+                var entities, pos, graphic;
+                entities = this.world.getEntities('position', 'graphic');
+
+                entities.forEach(function (entity) {
+                    pos = entity.getComponent('position');
+                    graphic = entity.getComponent('graphic');
+                    graphic.graphic.x = pos.x;
+                    graphic.graphic.y = pos.y;
+                });
+            }
+
+        });
+
+        var CollisionSystem = CES.System.extend({
+            update: function (dt)
+            {
+
+            }
+        });
+
+        /**
+         * Create the game world and add all the Entities
+         * and Systems.
+         */
+        this.world = new CES.World();
+        this.world.addEntity(this.player1);
+        this.world.addEntity(this.player2);
+        this.world.addEntity(this.ball);
+        this.world.addSystem(new InputSystem());
+        this.world.addSystem(new MovementSystem());
+        this.world.addSystem(new CollisionSystem());
+        this.world.addSystem(new GraphicSystem());
     },
     
     update: function()
     {
-        
+        this.world.update(this.time.elapsed/1000);
     }
 };
